1aa5f6453a8eec4cbe4776453d56afb196842445
GuyomeC <146094089+GuyomeC@users.noreply.github.com>
2025-09-03 10:52:40 +0200

    [UPDATE] Scene + CameraScript

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -1,4 +1,5 @@
 using System;
+using Unity.Cinemachine;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.Rendering;
@@ -6,14 +7,19 @@ using UnityEngine.Rendering;
 public class CameraScript : MonoBehaviour
 {
     [SerializeField] InputActionReference inputActionReference;
-    [SerializeField, Range(0,500)] float speed = 10f;
+    [SerializeField, Range(0, 500)] float speed = 10f;
+    [SerializeField] CinemachineCamera cam;
     
     private Vector2 moveInput;
     private Vector2 velocity;
 
     void Start()
     {
-        
+        CinemachineConfiner2D confiner = cam.GetComponent<CinemachineConfiner2D>();
+        BoxCollider2D boxCollider = confiner.BoundingShape2D as BoxCollider2D;
+
+        Debug.Log("Camera confiner: " + confiner);
+        Debug.Log("Camera collider: " + boxCollider);
     }
 
 
@@ -30,13 +36,12 @@ public class CameraScript : MonoBehaviour
     {
         moveInput = context.ReadValue<Vector2>();
         velocity = moveInput * speed;
-        transform.Translate(velocity * Time.deltaTime);
+        cam.transform.Translate(velocity * Time.deltaTime);
     }
 
     void Update()
     {
-        
         Vector2 move = new Vector2(moveInput.x, moveInput.y);
-        transform.Translate(move * speed * Time.deltaTime);
+        cam.transform.Translate(move * speed * Time.deltaTime);
     }
 }
