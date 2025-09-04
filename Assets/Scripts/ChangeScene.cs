a76d3c8875c1133f2d6d2b77ded0068ac3de2523
GuyomeC <146094089+GuyomeC@users.noreply.github.com>
2025-09-04 10:58:38 +0200

    [ADD] dontdestroy + changescene + update menu scene

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class ChangeScene : MonoBehaviour
+{
+    public void ChangeToScene(int sceneIndex)
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(sceneIndex);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
