e108c671af5fd95e39abdc83d3fa129db62a2def
GuyomeC <146094089+GuyomeC@users.noreply.github.com>
2025-09-04 09:46:01 +0200

    [ADD] audio manager script + audio test

---
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [Header("Audio Sources")]
+    [SerializeField] AudioSource musicSource;
+    [SerializeField] AudioSource sfxSource;
+
+    [Header("Audio Clips")]
+    public AudioClip backgroundMusic;
+    public AudioClip buttonClickSound;
+
+
+    private void Start()
+    {
+        musicSource.clip = backgroundMusic;
+        musicSource.loop = true;
+        musicSource.Play();
+    }
+
+    public void PlaySFX(AudioClip clip)
+    {
+        sfxSource.PlayOneShot(clip);
+    }
+}
